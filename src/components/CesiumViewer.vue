606cd8871af74e3b212485b407bbf6417d2dfd1d
QingMings <1821063757@qq.com>
2018-08-08 16:53:10 +0800

    init-commit

---
+++ src/components/CesiumViewer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="cesiumContainer">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CesiumViewer"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Cesium from 'cesium/Cesium'
+// noinspection ES6UnusedImports
+import widget from 'cesium/Widgets/widgets.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  mounted () {
+    var viewer = new Cesium.Viewer('cesiumContainer')
+  }
 }).$mount('#app')
