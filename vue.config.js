c878ebcd81ada730336fb92b6dafd360f80e3791
QingMings <1821063757@qq.com>
2018-08-14 23:46:55 +0800

    添加 gzip 插件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+//去console插件
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+//gzip压缩插件
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+// 拷贝文件插件
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const webpack = require('webpack')
 const path = require('path')
@@ -7,6 +12,8 @@ let cesiumSource = './node_modules/cesium/Source'
 let cesiumWorkers = '../Build/Cesium/Workers'
 module.exports = {
     baseUrl: '',
+    // 生产环境下 sourceMap
+    productionSourceMap: false,
     devServer: {
         port: 9999
     },
@@ -25,10 +32,32 @@ module.exports = {
             }
         },
         plugins: [
-            new CopyWebpackPlugin([ { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers'}]),
-            new CopyWebpackPlugin([ { from: path.join(cesiumSource, 'Assets'), to: 'Assets'}]),
-            new CopyWebpackPlugin([ { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets'}]),
-            new CopyWebpackPlugin([ { from: path.join(cesiumSource, 'ThirdParty/Workers'), to: 'ThirdParty/Workers'}]),
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                    compress: {
+                        warnings: false,
+                        drop_debugger: true,
+                        drop_console: true,
+                    },
+                },
+                sourceMap: false,
+                parallel: true,
+            }),
+            new CompressionWebpackPlugin({
+                asset: '[path].gz[query]',
+                algorithm: 'gzip',
+                test: new RegExp(
+                    '\\.(' +
+                    ['js', 'css'].join('|') +
+                    ')$',
+                ),
+                threshold: 10240,
+                minRatio: 0.8,
+            }),
+            new CopyWebpackPlugin([{from: path.join(cesiumSource, cesiumWorkers), to: 'Workers'}]),
+            new CopyWebpackPlugin([{from: path.join(cesiumSource, 'Assets'), to: 'Assets'}]),
+            new CopyWebpackPlugin([{from: path.join(cesiumSource, 'Widgets'), to: 'Widgets'}]),
+            new CopyWebpackPlugin([{from: path.join(cesiumSource, 'ThirdParty/Workers'), to: 'ThirdParty/Workers'}]),
             new webpack.DefinePlugin({
                 CESIUM_BASE_URL: JSON.stringify('./')
             })
@@ -38,6 +67,16 @@ module.exports = {
             unknownContextCritical: false
 
         }
+    },
+    css: {
+        // 启用 CSS modules
+        modules: false,
+        // 是否使用css分离插件
+        extract: true,
+        // 开启 CSS source maps?
+        sourceMap: false,
+        // css预设器配置项
+        loaderOptions: {}
     }
 }
 
